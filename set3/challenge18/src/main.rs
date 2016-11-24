c80edc5a5d0f1eaf264d6d7259d90c0c8bfadffa
Chris McDonald <mail.chris.mcdonald@gmail.com>
2016-11-24 08:35:41 -0700

    Challenge 18 init

---
+++ set3/challenge18/src/main.rs
@@ -0,0 +1,10 @@
+extern crate cryptobuddy;
+extern crate rustc_serialize;
+
+use rustc_serialize::base64::FromBase64;
+use cryptobuddy::{block, utils};
+
+
+fn main() {
+    println!("Hello, world!");
+}
